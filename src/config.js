757c1901c010fdf8c24e8b74a90c9851e14de9c4
Christopher <chrisobrien416@gmail.com>
2021-04-19 11:30:10 -0400

    first commit

---
+++ src/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  port: process.env.PORT || 8000,
+  NODE_ENV: process.env.NODE_ENV || "development",
+  ACCESS_KEY_ID: process.env.ACCESS_KEY_ID,
+  SECRET_ACCESS_KEY: process.env.SECRET_ACCESS_KEY,
+};
